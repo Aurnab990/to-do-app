628060bc39fd27d049296c0e465575f5c0c5a576
Aurnab Das <59371873+Aurnab990@users.noreply.github.com>
2022-07-03 13:43:53 +0600

    Header section designed

---
+++ src/Components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div style={{"backgroundColor":"#142453", "marginLeft":"270px", "margin":"90px","width":"680px","height":"340px","border":"20px","borderRadius":"20px"}} >
+            <div style={{"marginLeft":"20%","marginTop":"35px",}} className=''>
+            <h1  className="text-3xl text-white font-semibold">Welcome to UMe TO-DO-APP</h1>
+            <p></p>
+            <br></br>
+            <button className='bg-blue-400 p-1 w-2/4 lg:w-3/4 text-lg lg:text-xl'>Sign-in</button>
+            <br></br>
+            <br></br>
+            <button className='bg-blue-400 p-1 w-2/4 lg:w-3/4 text-xl'>Sign-up</button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Header;<h1>This is header</h1>
